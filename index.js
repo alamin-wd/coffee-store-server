fd10348dc0f05346b41b6aeee135ab31d2228004
alamin-wd <mdalamin67198@gmail.com>
2024-06-15 00:50:18 +0600

    50% With authentication

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 // Middleware
 app.use(cors());
@@ -48,6 +48,15 @@ app.get('/coffee', async (req, res) => {
     res.send(result);
 })
 
+app.get('/coffee/:id', async (req, res) => {
+
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) };
+    const result = await coffeeCollection.findOne(query);
+
+    res.send(result);
+})
+
 app.post('/coffee', async (req, res) => {
     const newCoffee = req.body;
     console.log(newCoffee);
@@ -59,6 +68,41 @@ app.post('/coffee', async (req, res) => {
         console.error('Error Inserting Coffee, error');
         res.json({ error: 'Failed to Add Coffee' });
     }
+})
+
+app.put('/coffee/:id', async (req, res) => {
+    const id = req.params.id;
+    const filter = { _id: new ObjectId(id) };
+    const options = { upsert: true };
+
+    const updatedCoffee = req.body;
+
+    const coffee = {
+
+        $set: {
+            name: updatedCoffee.name,
+            chef: updatedCoffee.chef,
+            supplier: updatedCoffee.supplier,
+            taste: updatedCoffee.taste,
+            category: updatedCoffee.category,
+            details: updatedCoffee.details,
+            price: updatedCoffee.price,
+            photoURL: updatedCoffee.photoURL
+        }
+
+    }
+
+    const result = await coffeeCollection.updateOne(filter, coffee, options);
+    res.send(result);
+})
+
+app.delete('/coffee/:id', async (req, res) => {
+
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) };
+    const result = await coffeeCollection.deleteOne(query);
+
+    res.send(result);
 })
 
 app.get('/', (req, res) => {
